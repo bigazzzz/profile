85d8ba39f3da47b7f8cd15d31ff0a7fea3f81c68
bigazzzz <bigazzzz@gmail.com>
2023-04-17 20:40:09 +0300

    update vim  readme

--- _vim_todo/install.sh
+++ _vim_todo/install.sh
@@ -9,7 +9,16 @@ wget -P /usr/share/fonts/truetype/anonymice https://raw.githubusercontent.com/ry
 wget -P /usr/share/fonts/truetype/anonymice https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/AnonymousPro/complete/Anonymice%20Nerd%20Font%20Complete%20Mono.ttf
 wget -P /usr/share/fonts/truetype/anonymice https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/AnonymousPro/complete/Anonymice%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf
 wget -P /usr/share/fonts/truetype/anonymice https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/AnonymousPro/complete/Anonymice%20Nerd%20Font%20Complete.ttf
+wget -P /usr/share/fonts/truetype/anonymice https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
+wget -P /usr/share/fonts/truetype/anonymice https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraMono.zip
+
+unzip /usr/share/fonts/truetype/anonymice/FiraCode.zip
+rm -f /usr/share/fonts/truetype/anonymice/FiraCode.zip
+
+unzip /usr/share/fonts/truetype/anonymice/FiraMono.zip
+rm -f /usr/share/fonts/truetype/anonymice/FiraMono.zip
+
 fc-cache -f -v
 
 # устанавливаем последний .vimrc
-wget -P ~ https://raw.githubusercontent.com/bigazzzz/vim-config/master/.vimrc
+cp .vimrc ~/.vimrc
